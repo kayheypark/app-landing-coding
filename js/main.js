fac3373e9017edf227ce6bb81c359ddbb5b8171b
kayheypark <59360980+kayheypark@users.noreply.github.com>
2020-06-09 03:31:55 +0900

    0609

--- js/main.js
+++ js/main.js
@@ -6,7 +6,7 @@ $(document).ready(function () {
     // Hide Header on on scroll down
     var didScroll;
     var lastScrollTop = 0;
-    var delta = 800;
+    var delta = 50;
     var navbarHeight = $('header').outerHeight();
     $(window).scroll(function (event) {
         didScroll = true;
@@ -56,7 +56,7 @@ $(document).ready(function () {
                 $("#about").removeClass("on");
             }
 
-            if (scroll >= 900 && scroll <= 15030) {
+            if (scroll >= 888 && scroll <= 15030) {
                 $(".about_bottom").addClass("on");
             } else {
                 $(".about_bottom").removeClass("on");
@@ -79,13 +79,82 @@ $(document).ready(function () {
             
             
             /*폰트*/
-            if (scroll >= 3000 && scroll <= 15030) {
+            if (scroll >= 2777 && scroll <= 15030) {
                 $(".font_wrap").addClass("on");
             } else {
                 $(".font_wrap").removeClass("on");
             }
 
+            /*아이콘*/
+                /*카테고리*/
+                if (scroll >= 3333 && scroll <= 15030) {
+                    $(".category").addClass("on");
+                } else {
+                    $(".category").removeClass("on");
+                }
 
+                /*드로워*/
+                if (scroll >= 3632 && scroll <= 15030) {
+                    $(".drawer").addClass("on");
+                } else {
+                    $(".drawer").removeClass("on");
+                }
+
+                /*기타*/
+                if (scroll >= 3840 && scroll <= 15030) {
+                    $(".etc").addClass("on");
+                } else {
+                    $(".etc").removeClass("on");
+                }
+
+            /*디테일*/
+            /*1. 스플래시 앤 메인*/
+            if (scroll >= 4730 && scroll <= 15030) {
+                $(".splash-and-main_wrap").addClass("on");
+            } else {
+                $(".splash-and-main_wrap").removeClass("on");
+            }
+            if (scroll >= 5777 && scroll <= 15030) {
+                $(".mockup_laydownPhone").addClass("on");
+            } else {
+                $(".mockup_laydownPhone").removeClass("on");
+            }
+            
+            
+            /*2. 알람 앤 캘린더*/
+            if (scroll >= 6895 && scroll <= 15030) {
+                $(".alarm-and-calender_wrap").addClass("on");
+            } else {
+                $(".alarm-and-calender_wrap").removeClass("on");
+            }
+      
+            /*3. 마이페이지 앤 채팅*/
+            if (scroll >= 8434 && scroll <= 15030) {
+                $(".mypage-and-chatting_wrap").addClass("on");
+            } else {
+                $(".mypage-and-chatting_wrap").removeClass("on");
+            }
+      
+            /*4. 지도*/
+            if (scroll >= 10010 && scroll <= 15030) {
+                $(".maps_wrap").addClass("on");
+            } else {
+                $(".maps_wrap").removeClass("on");
+            }
+      
+            /*5. 지도*/
+            if (scroll >= 11823 && scroll <= 15030) {
+                $(".row:last-of-type").addClass("on");
+            } else {
+                $(".row:last-of-type").removeClass("on");
+            }
+            
+            /*아웃트로*/
+            if (scroll >= 13113 && scroll <= 15030) {
+                $("#outro").addClass("on");
+            } else {
+                $("#outro").removeClass("on");
+            }
       
 
         });
