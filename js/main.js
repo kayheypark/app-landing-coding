a6840e06a311153f813da4a34638d0bedc96b11f
Park Gyeong Ho <59360980+kayheypark@users.noreply.github.com>
2020-06-08 18:29:53 +0900

    Update main.js

--- js/main.js
+++ js/main.js
@@ -6,7 +6,7 @@ $(document).ready(function () {
     // Hide Header on on scroll down
     var didScroll;
     var lastScrollTop = 0;
-    var delta = 5;
+    var delta = 800;
     var navbarHeight = $('header').outerHeight();
     $(window).scroll(function (event) {
         didScroll = true;
